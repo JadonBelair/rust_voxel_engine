d6ec95e7a9b945d3732c914de57df67caa509eaf
Jadon Belair <46850171+JadonBelair@users.noreply.github.com>
2025-07-31 01:03:26 -0400

    added texture struct

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use chunk::{Block, CHUNK_SIZE, Vertex};
 use chunk_manager::ChunkManager;
 use frustum::Frustum;
 use glam::{IVec3, Vec3};
-use wgpu::{PresentMode, util::DeviceExt};
+use texture::Texture;
+use wgpu::{util::DeviceExt, PresentMode};
 use winit::{
     application::ApplicationHandler,
     event::{DeviceEvent, KeyEvent, MouseButton, WindowEvent},
@@ -18,34 +19,7 @@ mod camera;
 mod chunk;
 mod chunk_manager;
 mod frustum;
-
-fn create_depth_texture(
-    device: &wgpu::Device,
-    width: u32,
-    height: u32,
-    label: Option<&str>,
-) -> (wgpu::Texture, wgpu::TextureView) {
-    let size = wgpu::Extent3d {
-        width,
-        height,
-        depth_or_array_layers: 1,
-    };
-
-    let texture = device.create_texture(&wgpu::TextureDescriptor {
-        label,
-        size,
-        mip_level_count: 1,
-        sample_count: 1,
-        dimension: wgpu::TextureDimension::D2,
-        format: wgpu::TextureFormat::Depth32Float,
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        view_formats: &[],
-    });
-
-    let view = texture.create_view(&wgpu::wgt::TextureViewDescriptor::default());
-
-    (texture, view)
-}
+mod texture;
 
 pub struct State {
     surface: wgpu::Surface<'static>,
@@ -66,8 +40,7 @@ pub struct State {
     camera_buffer: wgpu::Buffer,
     camera_bind_group: wgpu::BindGroup,
 
-    depth_texture: wgpu::Texture,
-    depth_texture_view: wgpu::TextureView,
+    depth_texture: Texture,
 }
 
 impl State {
@@ -224,8 +197,7 @@ impl State {
         let mut chunk_manager = ChunkManager::new(10);
         chunk_manager.update_around(IVec3::ZERO);
 
-        let (depth_texture, depth_texture_view) =
-            create_depth_texture(&device, size.width, size.height, Some("Depth Texture"));
+        let depth_texture = Texture::create_depth_texture(&device, size.width, size.height, Some("Depth Texture"));
 
         Ok(Self {
             surface,
@@ -247,7 +219,6 @@ impl State {
             camera_bind_group,
 
             depth_texture,
-            depth_texture_view,
         })
     }
 
@@ -258,9 +229,7 @@ impl State {
             self.surface.configure(&self.device, &self.config);
             self.projection.resize(width, height);
             self.is_surface_configured = true;
-
-            (self.depth_texture, self.depth_texture_view) =
-                create_depth_texture(&self.device, width, height, Some("Depth Texture"));
+            self.depth_texture = Texture::create_depth_texture(&self.device, width, height, Some("Depth Texture"));
         }
     }
 
@@ -372,7 +341,7 @@ impl State {
                     depth_slice: None,
                 })],
                 depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
-                    view: &self.depth_texture_view,
+                    view: &self.depth_texture.view,
                     depth_ops: Some(wgpu::Operations {
                         load: wgpu::LoadOp::Clear(1.0),
                         store: wgpu::StoreOp::Store,
